834858032bd5d8cb5df5e5f133beb22b4f2be28f
BRUNO ROGLIN <49683237+epifano-x@users.noreply.github.com>
2023-09-24 14:59:29 -0300

    quase

--- main.py
+++ main.py
@@ -2,9 +2,34 @@ import tkinter as tk
 from tkinter import filedialog
 import cv2
 from PIL import Image, ImageTk
+import numpy as np
 
-# Função para carregar uma imagem
+# Cria a janela principal
+janela = tk.Tk()
+janela.title("Ferramenta de Processamento de Imagem")
+
+# Lista para armazenar as imagens
+imagens = []
+
+# Variável para rastrear a imagem atual
+imagem_atual = None
+
+# Cria os frames para as colunas
+frame_esquerda = tk.Frame(janela, width=300)
+frame_direita = tk.Frame(janela)
+
+# Define o tamanho relativo das colunas
+frame_esquerda.grid(column=0, row=0, rowspan=2, sticky="nsew")
+frame_direita.grid(column=1, row=0, sticky="nsew")
+
+janela.grid_columnconfigure(0, weight=3)
+janela.grid_columnconfigure(1, weight=7)
+janela.grid_rowconfigure(0, weight=1)
+janela.grid_rowconfigure(1, weight=1)
+
 def carregar_imagem():
+    global imagens
+    global imagem_atual
     global imagem_label
     global info_label
 
@@ -31,9 +56,12 @@ def carregar_imagem():
             imagem_pil = Image.fromarray(imagem)
             imagem_tk = ImageTk.PhotoImage(imagem_pil)
 
-            # Atualiza o label da imagem
-            imagem_label.config(image=imagem_tk)
-            imagem_label.image = imagem_tk
+            # Adiciona a imagem à lista de imagens
+            imagens.append(imagem)
+
+            # Exibe a primeira imagem na lista
+            imagem_atual = imagens[0]
+            exibir_imagem(imagem_atual)
 
             # Exibe informações sobre a imagem
             info_label.config(text=f"Dimensões: {imagem.shape[1]}x{imagem.shape[0]}, "
@@ -43,23 +71,63 @@ def carregar_imagem():
             # Se a imagem não puder ser carregada, exibe uma mensagem de erro
             info_label.config(text="Erro ao carregar a imagem. Verifique o caminho do arquivo.")
 
+# Função para exibir uma imagem
+def exibir_imagem(imagem):
+    global imagem_label
+    global imagem_atual
 
-# Cria a janela principal
-janela = tk.Tk()
-janela.title("Visualizador de Imagem")
+    # Converte a imagem para o formato compatível com o Tkinter
+    imagem_pil = Image.fromarray(imagem)
+    imagem_tk = ImageTk.PhotoImage(imagem_pil)
 
-# Cria os frames para as colunas
-frame_esquerda = tk.Frame(janela, width=300)
-frame_direita = tk.Frame(janela)
+    # Atualiza o label da imagem
+    imagem_label.config(image=imagem_tk)
+    imagem_label.image = imagem_tk
 
-# Define o tamanho relativo das colunas
-frame_esquerda.grid(column=0, row=0, rowspan=2, sticky="nsew")
-frame_direita.grid(column=1, row=0, sticky="nsew")
+    # Atualiza a imagem atual
+    imagem_atual = imagem
 
-janela.grid_columnconfigure(0, weight=3)
-janela.grid_columnconfigure(1, weight=7)
-janela.grid_rowconfigure(0, weight=1)
-janela.grid_rowconfigure(1, weight=1)
+# Função para aplicar conversão de cor
+def converter_cor():
+    global imagem_atual
+    if imagem_atual is not None:
+        # Aplicar a conversão de cor (por exemplo, de RGB para escala de cinza)
+        imagem_atual = cv2.cvtColor(imagem_atual, cv2.COLOR_RGB2GRAY)
+        exibir_imagem(imagem_atual)
+
+# Função para aplicar filtro
+def aplicar_filtro():
+    global imagem_atual
+    if imagem_atual is not None:
+        # Aplicar o filtro desejado
+        # Por exemplo, um filtro de suavização (blur)
+        imagem_atual = cv2.blur(imagem_atual, (5, 5))  # Ajuste o tamanho do kernel conforme necessário
+        exibir_imagem(imagem_atual)
+
+# Função para aplicar detector de borda
+def detector_de_borda():
+    global imagem_atual
+    if imagem_atual is not None:
+        # Aplicar o detector de borda (por exemplo, usando o operador Canny)
+        imagem_atual = cv2.Canny(imagem_atual, 100, 200)  # Ajuste os limiares conforme necessário
+        exibir_imagem(imagem_atual)
+
+# Função para aplicar binarização
+def binarizar():
+    global imagem_atual
+    if imagem_atual is not None:
+        # Aplicar a binarização (por exemplo, usando a função threshold)
+        _, imagem_atual = cv2.threshold(imagem_atual, 128, 255, cv2.THRESH_BINARY)
+        exibir_imagem(imagem_atual)
+
+# Função para aplicar morfologia matemática
+def morfologia_matematica():
+    global imagem_atual
+    if imagem_atual is not None:
+        # Aplicar operações de morfologia matemática (por exemplo, erosão ou dilatação)
+        kernel = np.ones((5, 5), np.uint8)  # Ajuste o kernel conforme necessário
+        imagem_atual = cv2.erode(imagem_atual, kernel, iterations=1)
+        exibir_imagem(imagem_atual)
 
 # Botão para carregar a imagem
 botao_carregar = tk.Button(frame_esquerda, text="Carregar Imagem", command=carregar_imagem)
@@ -72,6 +140,22 @@ imagem_label.pack(expand=True, fill="both")
 # Label para exibir informações da imagem
 info_label = tk.Label(frame_direita, text="", anchor="w")
 info_label.pack(fill="x")
+
+# Adicionar botões para as operações
+botao_converter_cor = tk.Button(frame_esquerda, text="Converter Cor", command=converter_cor)
+botao_converter_cor.pack(pady=10)
+
+botao_aplicar_filtro = tk.Button(frame_esquerda, text="Aplicar Filtro", command=aplicar_filtro)
+botao_aplicar_filtro.pack()
+
+botao_detector_borda = tk.Button(frame_esquerda, text="Detector de Borda", command=detector_de_borda)
+botao_detector_borda.pack()
+
+botao_binarizar = tk.Button(frame_esquerda, text="Binarizar", command=binarizar)
+botao_binarizar.pack()
+
+botao_morfologia = tk.Button(frame_esquerda, text="Morfologia Matemática", command=morfologia_matematica)
+botao_morfologia.pack()
 
 # Inicia a interface gráfica
 janela.mainloop()
